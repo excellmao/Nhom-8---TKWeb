56f1956aa393aa6949a17da79f590b6865fc4687
excellmao <165754199+excellmao@users.noreply.github.com>
2025-04-26 22:08:12 +0700

    finally, im done

--- lepersonalities/ISTJ/ISTJ.js
+++ lepersonalities/ISTJ/ISTJ.js
@@ -53,3 +53,19 @@ const targetPosition = targetElement.offsetTop - headerHeight;
     });
   });
    
+// Toggles the mobile navigation menu
+function toggleMenu() {
+  const navTabs = document.getElementById('nav-tabs');
+  navTabs.classList.toggle('show');
+  }
+  
+  // Hides the menu when clicking outside of it
+  document.addEventListener('click', function (event) {
+  const navTabs = document.getElementById('nav-tabs');
+  const menuButton = document.querySelector('.menu-button');
+  
+  // Check if the click is outside the nav-tabs and menu-button
+  if (!navTabs.contains(event.target) && !menuButton.contains(event.target)) {
+      navTabs.classList.remove('show');
+  }
+  });
